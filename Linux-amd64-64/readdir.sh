94e99434cfaf20235d958ce4b8f7884a03547f6c
Roman Haefeli <reduzent@gmail.com>
2015-09-04 10:04:13 +0200

    fix version detection; use _common_build

--- Linux-amd64-64/readdir.sh
+++ Linux-amd64-64/readdir.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 library=readdir
-version=0.03~$(svn log | head -n2 | tail -n1 | cut -d" " -f1)
 platform=$(uname -s)
 arch=$(dpkg --print-architecture)
 bit=64
 pdsrcdir=/home/roman/pd-src
 librarydir=${pdsrcdir}/externals/moocow/${library}
+version=0.03~$(cd $librarydir; svn log | head -n2 | tail -n1 | cut -d" " -f1)
 pddir=${pdsrcdir}/pd/
 workspacedir=${pdsrcdir}/workspace/${platform}-${arch}-${bit}
 remoteworkspacedir=${workspacedir}
@@ -24,18 +24,6 @@ cp readdir.patch $librarydir
   
   # apply patch
   patch -p0 < fix-readdir.patch
-  
-  # build
-  if ! ( make PD_PATH=${pddir} && \
-         make pkglibdir=${workspacedir} install )
-  then
-    echo "error while building"
-    exit 1
-  fi
-  
-  # create version-file
-  echo "$version" > ${workspacedir}/${library}/VERSION
-  
-  # copy
-  rsync -av --delete ${workspacedir}/${library} ${remotehost}:${remoteworkspacedir}/.
 )
+
+. _common_build
