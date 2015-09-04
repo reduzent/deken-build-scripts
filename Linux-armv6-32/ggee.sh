1a5c9a611e97cd37101fe5c2bc6f4aad88c3996b
Roman Haefeli <reduzent@gmail.com>
2015-09-04 15:10:47 +0200

    builds for armv6

---
+++ Linux-armv6-32/ggee.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+library=ggee
+pdsrcdir=/home/roman/pd-src
+librarydir=${pdsrcdir}/externals/${library}
+version=$(cat ${librarydir}/VERSION)~$(cd $librarydir; svn log | head -n2 | tail -n1 | cut -d" " -f1)
+pddir=${pdsrcdir}/pd
+platform=$(basename $(pwd))
+workspacedir=${pdsrcdir}/workspace/${platform}
+remoteworkspacedir=${workspacedir}
+remotehost=netpd.org
+
+. _common_build
---
+++ Linux-armv6-32/slipdec.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+library=slipdec
+pdsrcdir=/home/roman/pd-src
+librarydir=${pdsrcdir}/externals/mrpeach/${library}
+version=0.1~$(cd $librarydir; svn log | head -n2 | tail -n1 | cut -d" " -f1)
+pddir=${pdsrcdir}/pd/
+platform=$(basename $(pwd))
+workspacedir=${pdsrcdir}/workspace/${platform}
+remoteworkspacedir=${workspacedir}
+remotehost=netpd.org
+
+cp ${library}.patch $librarydir
+
+(
+  cd $librarydir
+  
+  # clean area
+  rm -rf LICENSE.txt README.txt slipdec-meta.pd Makefile
+  
+  # apply patch
+  patch -p0 < ${library}.patch
+
+  make clean
+)
+
+. _common_build
