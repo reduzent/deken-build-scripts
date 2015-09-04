cd3e8d8b1a280a7b2950c89ed9059ff3cd803d34
Roman Haefeli <reduzent@gmail.com>
2015-09-04 19:26:38 +0200

    use folder name to detect platform

--- Linux-i386-32/slipenc.sh
+++ Linux-i386-32/slipenc.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 library=slipenc
 platform=$(uname -s)
-arch=$(dpkg --print-architecture)
 bit=32
 pdsrcdir=/home/roman/pd-src
 librarydir=${pdsrcdir}/externals/mrpeach/${library}
 version=0.1~$(cd $librarydir; svn log | head -n2 | tail -n1 | cut -d" " -f1)
 pddir=${pdsrcdir}/pd/
-workspacedir=${pdsrcdir}/workspace/${platform}-${arch}-${bit}
+platform=$(basename $(pwd))
+workspacedir=${pdsrcdir}/workspace/${platform}
 remoteworkspacedir=${workspacedir}
 remotehost=roman@netpd.org
 
