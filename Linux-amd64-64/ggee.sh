9b800930d1b92d6b27a8034053b14bb9bb89e653
Roman Haefeli <reduzent@gmail.com>
2015-09-04 09:58:35 +0200

    fix version detection; use _common_build

--- Linux-amd64-64/ggee.sh
+++ Linux-amd64-64/ggee.sh
@@ -1,34 +1,14 @@
 #!/bin/bash
 library=ggee
-version=$(cat VERSION)
 platform=$(uname -s)
 arch=$(dpkg --print-architecture)
 bit=64
 pdsrcdir=/home/roman/pd-src
 librarydir=${pdsrcdir}/externals/${library}
+version=$(cat ${librarydir}/VERSION)
 pddir=${pdsrcdir}/pd/
 workspacedir=${pdsrcdir}/workspace/${platform}-${arch}-${bit}
 remoteworkspacedir=${workspacedir}
 remotehost=netpd.org
 
-
-(
-  cd $librarydir
-  # clean area
-  rm -rf ${workspacedir}/${library}
-  make clean
-  
-  # build
-  if ! ( make PD_PATH=${pddir} && \
-         make pkglibdir=${workspacedir} install )
-  then
-    echo "error while building"
-    exit 1
-  fi
-  
-  # create version-file
-  echo "$version" > ${workspacedir}/${library}/VERSION
-  
-  # copy
-  rsync -av --delete ${workspacedir}/${library} ${remotehost}:${remoteworkspacedir}/.
-)
+. _common_build
