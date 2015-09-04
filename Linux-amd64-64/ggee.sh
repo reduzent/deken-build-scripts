1c5aecaddc96e0e216179739a931153c0e1e6f9f
Roman Haefeli <reduzent@gmail.com>
2015-09-04 08:58:11 +0200

    build for ggee on Linux-amd64

---
+++ Linux-amd64-64/ggee.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+library=ggee
+platform=$(uname -s)
+arch=$(dpkg --print-architecture)
+bit=64
+pdsrcdir=/home/roman/pd-src
+librarydir=${pdsrcdir}/externals/${library}
+version=$(cat VERSION)
+pddir=${pdsrcdir}/pd/
+workspacedir=${pdsrcdir}/workspace/${platform}-${arch}-${bit}
+remoteworkspacedir=${workspacedir}
+remotehost=netpd.org
+
+
+(
+  cd $librarydir
+  # clean area
+  make clean
+  
+  # build
+  if ! ( make PD_PATH=${pddir} && \
+         make pkglibdir=${workspacedir} install )
+  then
+    echo "error while building"
+    exit 1
+  fi
+  
+  # create version-file
+  echo "$version" > ${workspacedir}/${library}/VERSION
+  
+  # copy
+  rsync -av --delete ${workspacedir}/${library} ${remotehost}:${remoteworkspacedir}/.
+)
