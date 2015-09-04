7862245bb2729f5a56b108276037fd64f0a9309e
Roman Haefeli <reduzent@gmail.com>
2015-09-04 10:01:50 +0200

    fix version detection; use _common_build

--- Linux-amd64-64/osc.sh
+++ Linux-amd64-64/osc.sh
@@ -1,36 +1,14 @@
 #!/bin/bash
 library=osc
-version=0.2~$(svn log | head -n2 | tail -n1 | cut -d" " -f1)
 platform=$(uname -s)
 arch=$(dpkg --print-architecture)
 bit=64
 pdsrcdir=/home/roman/pd-src
 librarydir=${pdsrcdir}/externals/mrpeach/${library}
+version=0.2~$(cd $librarydir; svn log | head -n2 | tail -n1 | cut -d" " -f1)
 pddir=${pdsrcdir}/pd/
 workspacedir=${pdsrcdir}/workspace/${platform}-${arch}-${bit}
 remoteworkspacedir=${workspacedir}
 remotehost=netpd.org
 
-
-(
-  cd $librarydir
-
-  # clean area
-  rm -rf ${workspacedir}/${library}
-  make clean
-  
-  # build
-  if ! ( make PD_PATH=${pddir} && \
-         make pkglibdir=${workspacedir} install )
-  then
-    echo "error while building"
-    exit 1
-  fi
-  
-  # create version-file
-  echo "$version" > ${workspacedir}/${library}/VERSION
-  
-  # copy
-  rsync -av --delete ${workspacedir}/${library} ${remotehost}:${remoteworkspacedir}/.
-)
-
+. _common_build
