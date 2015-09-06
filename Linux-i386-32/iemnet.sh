482c3e50b4eb94872926ffe8c1bc84485a56c21f
Roman Haefeli <reduzent@gmail.com>
2015-09-06 22:20:24 +0200

    new release: 0.2.1

--- Linux-i386-32/iemnet.sh
+++ Linux-i386-32/iemnet.sh
@@ -3,6 +3,6 @@
 
 library=iemnet
 librarydir=${pdsrcdir}/pd-${library}
-version=0.2~git$(cd $librarydir; date -d @$(git log -1 --pretty=format:%ct) +%Y%m%d)
+version=0.2.1~git$(cd $librarydir; date -d @$(git log -1 --pretty=format:%ct) +%Y%m%d)
 
 . _common_build
