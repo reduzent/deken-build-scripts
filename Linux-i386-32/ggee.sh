ede5ed1e1c980fa3963d80d853ba2821e3862f8f
Roman Haefeli <reduzent@gmail.com>
2015-09-06 22:04:36 +0200

    apply patch to exclude sfwrite from compiling

--- Linux-i386-32/ggee.sh
+++ Linux-i386-32/ggee.sh
@@ -5,4 +5,16 @@ library=ggee
 librarydir=${pdsrcdir}/externals/${library}
 version=$(cat ${librarydir}/VERSION)~$(cd $librarydir; svn log | head -n2 | tail -n1 | cut -d" " -f1)
 
+cp ${library}.patch $librarydir
+
+(
+  cd $librarydir
+  
+  # revert changes
+  svn revert -R .
+  
+  # apply patch
+  patch -p0 < ${library}.patch
+)
+
 . _common_build
