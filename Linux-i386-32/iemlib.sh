01763057bb1272a431c02c7a3e4490b2910d07d9
Roman Haefeli <reduzent@gmail.com>
2015-10-06 14:44:37 +0200

    make MAKEFLAGS platform-independent

--- Linux-i386-32/iemlib.sh
+++ Linux-i386-32/iemlib.sh
@@ -4,6 +4,6 @@
 library=iemlib
 librarydir=${pdsrcdir}/externals/${library}
 version=1.17~$(cd $librarydir; svn log | head -n2 | tail -n1 | cut -d" " -f1)
-MAKEFLAGS='INSTALL_BIN=../../workspace/Linux-i386-32/iemlib INSTALL_DOC=../../workspace/Linux-i386-32/iemlib PDSOURCE=/home/roman/pd-src/pd/src'
+MAKEFLAGS="INSTALL_BIN=../../workspace/${platform}/iemlib INSTALL_DOC=../../workspace/${platform}/iemlib PDSOURCE=/home/roman/pd-src/pd/src"
 
 . _common_build
