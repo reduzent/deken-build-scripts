ca562d3341408a419781dfe9f604f5e1a35992f4
Roman Haefeli <reduzent@gmail.com>
2015-11-16 17:19:13 +0100

    remove the patching part

--- Linux-i386-32/slip.sh
+++ Linux-i386-32/slip.sh
@@ -2,23 +2,8 @@
 . _common_vars
 
 library=slip
-librarydir=${pdsrcdir}/externals/mrpeach/${library}
-version=0.1~$(cd ${pdsrcdir}/externals/mrpeach/slipdec; svn log | head -n2 | tail -n1 | cut -d" " -f1)
-
-# create build layout
-mkdir -p $librarydir
-cp ${library}.patch $librarydir
-
-(
-  cd $librarydir
-  ln -s ../slipdec/* .
-  ln -s ../slipenc/* . 
-  
-  # apply patch
-  patch -p0 < ${library}.patch
-)
+librarydir=${pdsrcdir}/mrpeach/${library}
+version=0.1
 
 . _common_build
 
-# clean up
-rm -rf $librarydir
