c04978981a64ca76cd86274c2d8d79ee78be71fc
Roman Haefeli <reduzent@gmail.com>
2015-11-16 15:33:38 +0100

    fix readdir root path; assume pd-lib-builder Makefile

--- Linux-i386-32/readdir.sh
+++ Linux-i386-32/readdir.sh
@@ -2,22 +2,7 @@
 . _common_vars
 
 library=readdir
-librarydir=${pdsrcdir}/externals/moocow/${library}
-version=0.03~$(cd $librarydir; svn log | head -n2 | tail -n1 | cut -d" " -f1)
-
-cp ${library}.patch $librarydir
-
-(
-  cd $librarydir
-  
-  # clean area
-  rm -rf ${workspacedir}/${library}
-  make clean
-  svn revert -R .
-  rm -rf readdir-meta.pd Makefile
-  
-  # apply patch
-  patch -p0 < readdir.patch
-)
+librarydir=${pdsrcdir}/moocow/${library}
+version=0.02
 
 . _common_build
