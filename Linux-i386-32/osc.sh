fea828efb7c9241d22e6e227d5717c326b699c5a
Roman Haefeli <reduzent@gmail.com>
2015-11-16 15:31:46 +0100

    fix osc root dir

--- Linux-i386-32/osc.sh
+++ Linux-i386-32/osc.sh
@@ -2,7 +2,7 @@
 . _common_vars
 
 library=osc
-librarydir=${pdsrcdir}/externals/mrpeach/${library}
-version=0.2~$(cd $librarydir; svn log | head -n2 | tail -n1 | cut -d" " -f1)
+librarydir=${pdsrcdir}/mrpeach/${library}
+version=0.2
 
 . _common_build
