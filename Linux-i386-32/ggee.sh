943adbf111fe48af998dc6ef907c91bd3451c5b2
Roman Haefeli <reduzent@gmail.com>
2015-11-16 15:31:16 +0100

    do not apply patches and fix ggee root dir

--- Linux-i386-32/ggee.sh
+++ Linux-i386-32/ggee.sh
@@ -2,19 +2,8 @@
 . _common_vars
 
 library=ggee
-librarydir=${pdsrcdir}/externals/${library}
-version=$(cat ${librarydir}/VERSION)~$(cd $librarydir; svn log | head -n2 | tail -n1 | cut -d" " -f1)
+librarydir=${pdsrcdir}/${library}
+version=$(cat ${librarydir}/VERSION)
 
-cp ${library}.patch $librarydir
-
-(
-  cd $librarydir
-  
-  # revert changes
-  svn revert -R .
-  
-  # apply patch
-  patch -p0 < ${library}.patch
-)
 
 . _common_build
