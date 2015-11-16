3b195773e8ed88d753469fff4fd5293e47ceca9e
Roman Haefeli <reduzent@gmail.com>
2015-11-16 20:07:31 +0100

    add MAKEFLAG to include pd src dir for old-school libdir-Makefile

--- Linux-i386-32/iemnet.sh
+++ Linux-i386-32/iemnet.sh
@@ -4,5 +4,6 @@
 library=iemnet
 librarydir=${pdsrcdir}/pd-${library}
 version=0.2.1~git$(cd $librarydir; date -d @$(git log -1 --pretty=format:%ct) +%Y%m%d)
+MAKEFLAGS="PD_INCLUDE=../pd/src/"
 
 . _common_build
