aa33502c55c6ed1e2ddf2c34b305048525dd8b70
Roman Haefeli <reduzent@gmail.com>
2015-11-18 12:37:03 +0100

    automatically retrieve version from meta and git log

--- Linux-i386-32/binfile.sh
+++ Linux-i386-32/binfile.sh
@@ -3,6 +3,11 @@
 
 library=binfile
 librarydir=${pdsrcdir}/mrpeach/${library}
-version=0.1
+# Extract version from meta file and append git ~version
+version=$(sed -n \
+      's|^\#X text [0-9][0-9]* [0-9][0-9]* VERSION \(.*\);|\1|p' \
+      ${librarydir}/${library}-meta.pd\
+      )~git$(cd $librarydir; \
+      date -d @$(git log -1 --pretty=format:%ct) +%Y%m%d)
 
 . _common_build
