f2a96d82616efa9257f2075fa89bf57cb7d1918c
Roman Haefeli <reduzent@gmail.com>
2015-12-14 23:08:51 +0100

    put date command into variable

--- Linux-i386-32/ggee.sh
+++ Linux-i386-32/ggee.sh
@@ -7,7 +7,7 @@ version=$(sed -n \
       's|^\#X text [0-9][0-9]* [0-9][0-9]* VERSION \(.*\);|\1|p' \
       ${librarydir}/${library}-meta.pd\
       )~git$(cd $librarydir; \
-      date -d @$(git log -1 --pretty=format:%ct) +%Y%m%d)
+      ${datecommand}$(git log -1 --pretty=format:%ct) +%Y%m%d)
 
 
 . _common_build
