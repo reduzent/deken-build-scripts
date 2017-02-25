38a134d3d2014706ce93678a6c601531b93bcdcb
Roman Haefeli <reduzent@gmail.com>
2017-02-25 20:54:00 +0100

    added build for comport (works for Linux-amd64)

---
+++ Linux-i386-32/comport.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+. _common_vars
+
+library=comport
+librarydir=${pdsrcdir}/pd-comport/${library}
+# Extract version from meta file and append git ~version
+version=$(sed -n \
+      's|^\#X text [0-9][0-9]* [0-9][0-9]* VERSION \(.*\);|\1|p' \
+      ${librarydir}/${library}-meta.pd\
+      )~git$(cd $librarydir; \
+      ${datecommand}$(git log -1 --pretty=format:%ct) +%Y%m%d)
+
+. _common_build
