c937a4125cd68a68a2943e19916c51852daa05ea
Roman Haefeli <reduzent@gmail.com>
2017-02-27 22:30:10 +0100

    use new path; don't append git-revision to version

--- Linux-i386-32/comport.sh
+++ Linux-i386-32/comport.sh
@@ -2,12 +2,10 @@
 . _common_vars
 
 library=comport
-librarydir=${pdsrcdir}/pd-comport/${library}
+librarydir=${pdsrcdir}/${library}
 # Extract version from meta file and append git ~version
 version=$(sed -n \
       's|^\#X text [0-9][0-9]* [0-9][0-9]* VERSION \(.*\);|\1|p' \
-      ${librarydir}/${library}-meta.pd\
-      )~git$(cd $librarydir; \
-      ${datecommand}$(git log -1 --pretty=format:%ct) +%Y%m%d)
+      ${librarydir}/${library}-meta.pd)
 
 . _common_build
