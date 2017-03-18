8d6ba6d37aaf05143f0a152b5ae8213191c62ea2
Roman Haefeli <reduzent@gmail.com>
2017-03-18 22:36:39 +0100

    show versions of all archs before proceeding

--- deken-upload.sh
+++ deken-upload.sh
@@ -11,6 +11,25 @@ fi
 # Get version of package series 
 version="$(cat ${workspacedir}/Sources/${library}/VERSION.txt)"
 
+# display version of package for each arch
+for arch in $(find ${workspacedir} -maxdepth 1 -mindepth 1 -type d)
+do
+  echo -ne "$(basename ${arch}):      \t"
+  if [ -f "${workspacedir}/${arch}/${library}/VERSION.txt" ]
+  then
+    echo "v$(cat ${workspacedir}/${arch}/${library}/VERSION.txt)"
+  else
+    echo "None"
+  fi
+done
+
+read -p "Proceed? Y/n " -n 1 proceed
+echo
+if [[ "$proceed" =~ [Nn] ]]
+then 
+  exit 1
+fi
+
 # create packages
 for arch in $(find ${workspacedir} -maxdepth 1 -mindepth 1 -type d)
 do
