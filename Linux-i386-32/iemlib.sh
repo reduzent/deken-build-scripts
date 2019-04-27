db147404ff988ddd98aed2cb3d4d970d54f94ae1
Roman Haefeli <reduzent@gmail.com>
2019-04-27 22:25:27 +0200

    iemlib.sh: use new base path

--- Linux-i386-32/iemlib.sh
+++ Linux-i386-32/iemlib.sh
@@ -2,7 +2,7 @@
 . _common_vars
 
 library=iemlib
-librarydir=${pdsrcdir}/externals/${library}
+librarydir=${pdsrcdir}/${library}
 version=1.17~$(cd $librarydir; svn log | head -n2 | tail -n1 | cut -d" " -f1)
 MAKEFLAGS="INSTALL_BIN=../../workspace/${platform}/iemlib INSTALL_DOC=../../workspace/${platform}/iemlib PDSOURCE=/home/roman/pd-src/pd/src"
 
