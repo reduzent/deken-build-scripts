cf46df2a742fd0a2b3976a213eaff0cbb914e33f
Roman Haefeli <reduzent@gmail.com>
2019-04-28 21:58:23 +0200

    use version from meta file and skip git last commit date

--- Linux-i386-32/iemnet.sh
+++ Linux-i386-32/iemnet.sh
@@ -2,11 +2,10 @@
 . _common_vars
 
 library=iemnet
-librarydir=${pdsrcdir}/pd-${library}
+librarydir=${pdsrcdir}/${library}
 version=$(sed -n \
       's|^\#X text [0-9][0-9]* [0-9][0-9]* VERSION \(.*\);|\1|p' \
       ${librarydir}/${library}-meta.pd\
-      )~git$(cd $librarydir; \
-      ${datecommand}$(git log -1 --pretty=format:%ct) +%Y%m%d)
+      )
 
 . _common_build
