ff96b53346e91df20b73154d135dcf0a0273705a
Roman Haefeli <reduzent@gmail.com>
2021-03-17 16:46:37 +0100

    ggee: do not use git commit in version

--- Linux-i386-32/ggee.sh
+++ Linux-i386-32/ggee.sh
@@ -6,8 +6,7 @@ librarydir=${pdsrcdir}/${library}
 version=$(sed -n \
       's|^\#X text [0-9][0-9]* [0-9][0-9]* VERSION \(.*\);|\1|p' \
       ${librarydir}/${library}-meta.pd\
-      )~git$(cd $librarydir; \
-      ${datecommand}$(git log -1 --pretty=format:%ct) +%Y%m%d)
+      )
 
 
 . _common_build
