f0316dff2dcb9802231e152b91fdd0a5243bae79
Roman Haefeli <reduzent@gmail.com>
2022-04-28 14:02:05 +0200

    update iemlib build script

--- Linux-i386-32/iemlib.sh
+++ Linux-i386-32/iemlib.sh
@@ -3,7 +3,6 @@
 
 library=iemlib
 librarydir=${pdsrcdir}/${library}
-version=1.22~git$(cd $librarydir; ${datecommand}$(git log -1 --pretty=format:%ct) +%Y%m%d)
-MAKEFLAGS="INSTALL_BIN=../../workspace/${platform}/iemlib INSTALL_DOC=../../workspace/${platform}/iemlib PDSOURCE=/home/roman/pd-src/pd/src"
+version=1.22
 
 . _common_build
