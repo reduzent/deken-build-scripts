b594fdd2020c6719ace9755f4e6ba3996b95be6c
Roman Haefeli <reduzent@gmail.com>
2022-04-28 14:02:55 +0200

    midifile.sh: remove git commit from version string

--- Linux-i386-32/midifile.sh
+++ Linux-i386-32/midifile.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 . _common_vars
 
-library=binfile
-librarydir=${pdsrcdir}/mrpeach/${library}
+library=midifile
+librarydir=${pdsrcdir}/${library}
 # Extract version from meta file and append git ~version
 version=$(sed -n \
       's|^\#X text [0-9][0-9]* [0-9][0-9]* VERSION \(.*\);|\1|p' \
       ${librarydir}/${library}-meta.pd\
-      )~git$(cd $librarydir; \
-      ${datecommand}$(git log -1 --pretty=format:%ct) +%Y%m%d)
-
+      )
 . _common_build
