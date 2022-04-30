a9fbd0d166445f013e41c75407fad1a4467cdda8
Roman Haefeli <reduzent@gmail.com>
2022-04-30 14:52:54 +0200

    binfile, osc, slip: use their own repos instead of mrpeach

--- Linux-i386-32/binfile.sh
+++ Linux-i386-32/binfile.sh
@@ -2,7 +2,7 @@
 . _common_vars
 
 library=binfile
-librarydir=${pdsrcdir}/mrpeach/${library}
+librarydir=${pdsrcdir}/${library}
 # Extract version from meta file and append git ~version
 version=$(sed -n \
       's|^\#X text [0-9][0-9]* [0-9][0-9]* VERSION \(.*\);|\1|p' \
