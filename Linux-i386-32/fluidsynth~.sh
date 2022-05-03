55a4dc5a195dd54840c350428d9cec9cb37a771e
Roman Haefeli <reduzent@gmail.com>
2022-05-03 17:04:18 +0200

    update fluidsynth.sh script

--- Linux-i386-32/fluidsynth~.sh
+++ Linux-i386-32/fluidsynth~.sh
@@ -2,7 +2,10 @@
 . _common_vars
 
 library=fluidsynth~
-librarydir=${pdsrcdir}/pd-fluidsynth
-# Extract version from meta file and append git ~version
-version=~20200110
+librarydir=${pdsrcdir}/fluidsynth
+version=$(sed -n \
+      's|^\#X text [0-9][0-9]* [0-9][0-9]* VERSION \(.*\);|\1|p' \
+      ${librarydir}/${library}-meta.pd)
+localdep=true
+
 . _common_build
