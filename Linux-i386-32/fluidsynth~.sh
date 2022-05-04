be913e1427c772cfe047b04bf61443c81b9ae1c7
Roman Haefeli <reduzent@gmail.com>
2022-05-04 09:32:20 +0200

    fluidsynth~: add flag for using local libfluidsynth

--- Linux-i386-32/fluidsynth~.sh
+++ Linux-i386-32/fluidsynth~.sh
@@ -7,5 +7,6 @@ version=$(sed -n \
       's|^\#X text [0-9][0-9]* [0-9][0-9]* VERSION \(.*\);|\1|p' \
       ${librarydir}/${library}-meta.pd)
 localdep=true
+MAKEFLAGS="$MAKEFLAGS LD_LIBRARY_PATH=/home/roman/.local/lib"
 
 . _common_build
