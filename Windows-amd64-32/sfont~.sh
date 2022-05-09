282c89b566b70e8422dadb67fee986db7276a52f
Roman Haefeli <reduzent@gmail.com>
2022-05-09 17:33:51 +0200

    add proper MAKEFLAGS for sfont~ on Linux-amd64-32

--- Windows-amd64-32/sfont~.sh
+++ Windows-amd64-32/sfont~.sh
@@ -7,6 +7,6 @@ version=$(sed -n \
       's|^\#X text [0-9][0-9]* [0-9][0-9]* VERSION \(.*\);|\1|p' \
       ${librarydir}/${library}-meta.pd)
 localdep=true
-MAKEFLAGS="$MAKEFLAGS LD_LIBRARY_PATH=/home/roman/.local/lib"
+MAKEFLAGS="$MAKEFLAGS LD_LIBRARY_PATH=/home/roman/.local/lib LDFLAGS+=-L/home/roman/.local/lib CPPFLAGS+=-I/home/roman/.local/include"
 
 . _common_build
