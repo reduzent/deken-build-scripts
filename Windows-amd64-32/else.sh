e70c6c9cd93df60048f1541821f3c2783791b0cc
Roman Haefeli <reduzent@gmail.com>
2022-05-09 23:32:08 +0200

    rename pd-else to else

--- Windows-amd64-32/else.sh
+++ Windows-amd64-32/else.sh
@@ -2,7 +2,7 @@
 . _common_vars
 
 library="else"
-librarydir=${pdsrcdir}/pd-${library}
+librarydir=${pdsrcdir}/${library}
 version=$(sed -n \
       's|^\#X text [0-9][0-9]* [0-9][0-9]* VERSION \(.*\);|\1|p' \
       ${librarydir}/${library}-meta.pd)
